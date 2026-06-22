b80c0aa570157fef6357a3d4e407cbe6b3e95a5b
Joyson <antonyjoyson.dsouza@omnireach.com>
2026-06-22 15:18:12 +0530

    feat - auth

--- src/app/feature/auth/pages/login/login.ts
+++ src/app/feature/auth/pages/login/login.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login implements OnInit {
+  constructor(private loginFormBuilder: FormBuilder) {}
+
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.loginFormBuilder.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const formData: LoginFormValues = this.loginForm.getRawValue();
+
+    console.log(formData);
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
--- src/app/feature/auth/pages/register/register.ts
+++ src/app/feature/auth/pages/register/register.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register implements OnInit {
+  constructor(private registerFormBuilder: FormBuilder) {}
+
+  registerForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.registerForm = this.registerFormBuilder.nonNullable.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      contactNumber: ['', [Validators.required]],
+      isRecruiter: [false],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    const formData = this.registerForm.getRawValue();
+
+    console.log('REGISTER DATA:', formData);
+
+    const role = formData.isRecruiter ? 'recruiter' : 'job_seeker';
+
+    console.log('ROLE:', role);
+
+    this.registerForm.reset({
+      isRecruiter: false,
+    });
+  }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  get contactNumber() {
+    return this.registerForm.get('contactNumber');
+  }
+}
