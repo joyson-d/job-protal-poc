cde2fb85aa4ee10ec53e59ae4b8f02eddbd076ad
Joyson <antonyjoyson.dsouza@omnireach.com>
2026-07-08 15:57:08 +0530

    feat - saved jobs empty screen added

--- src/app/feature/saved-jobs/saved-jobs.ts
+++ src/app/feature/saved-jobs/saved-jobs.ts
@@ -2,18 +2,16 @@ import { Component, computed } from '@angular/core';
 import { JobActivityStore } from '../../core/job-activity/job-activity-store';
 import { JobActivityService } from '../../core/job-activity/job-activity-service';
 import { JobCard } from '../../shared/components/job-card/job-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-saved-jobs',
-  imports: [JobCard],
+  imports: [RouterLink, JobCard],
   templateUrl: './saved-jobs.html',
   styleUrl: './saved-jobs.css',
 })
 export class SavedJobs {
+  constructor(private jobActivityService: JobActivityService) {}
 
-  constructor(private jobActivityService:JobActivityService){
-
-  }
-
-  savedJobs=computed(()=> this.jobActivityService.getSavedJobsList)
+  savedJobs = computed(() => this.jobActivityService.getSavedJobsList);
 }
