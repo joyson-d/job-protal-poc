bd92bada9534cedab89b57cdd03493b022f33970
Joyson <antonyjoyson.dsouza@overtminds.com>
2026-07-14 17:12:58 +0530

    profile tabs- translate

--- src/app/feature/profile/profile.ts
+++ src/app/feature/profile/profile.ts
@@ -3,6 +3,7 @@ import { Overview } from './overview/overview';
 import { Education } from './education/education';
 import { Experience } from './experience/experience';
 import { Skills } from './skills/skills';
+import { TranslatePipe } from '@ngx-translate/core';
 
 const profileOptions = ['overview', 'skills', 'education', 'experience'] as const;
 
@@ -10,21 +11,29 @@ type profileOptionTypes = (typeof profileOptions)[number];
 
 type tabsArrType = {
   key: profileOptionTypes;
-  label: string;
 };
 
 @Component({
   selector: 'app-profile',
-  imports: [Overview, Education, Experience, Skills],
+  imports: [Overview, Education, Experience, Skills,TranslatePipe],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile {
+  // tabs: tabsArrType[] = [
+  //   { key: 'overview', label: 'Personal Info' },
+  //   { key: 'skills', label: 'Skills' },
+  //   { key: 'education', label: 'Education' },
+  //   { key: 'experience', label: 'Experience' },
+  // ];
+
+  // activeTab: profileOptionTypes = 'overview';
+
   tabs: tabsArrType[] = [
-    { key: 'overview', label: 'Personal Info' },
-    { key: 'skills', label: 'Skills' },
-    { key: 'education', label: 'Education' },
-    { key: 'experience', label: 'Experience' },
+    { key: 'overview' },
+    { key: 'skills' },
+    { key: 'education' },
+    { key: 'experience' },
   ];
 
   activeTab: profileOptionTypes = 'overview';
