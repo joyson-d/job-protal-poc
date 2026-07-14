d498938a2faf4b6dc757fe718946d29c00ea8ac6
Joyson <antonyjoyson.dsouza@overtminds.com>
2026-07-14 17:06:45 +0530

    saved jobs translate

--- src/app/feature/saved-jobs/saved-jobs.ts
+++ src/app/feature/saved-jobs/saved-jobs.ts
@@ -3,10 +3,11 @@ import { JobActivityStore } from '../../core/job-activity/job-activity-store';
 import { JobActivityService } from '../../core/job-activity/job-activity-service';
 import { JobCard } from '../../shared/components/job-card/job-card';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-saved-jobs',
-  imports: [RouterLink, JobCard],
+  imports: [RouterLink, JobCard,TranslatePipe],
   templateUrl: './saved-jobs.html',
   styleUrl: './saved-jobs.css',
 })
