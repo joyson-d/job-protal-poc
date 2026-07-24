bd5ba2bca3818aea140fe1b7d2cd9475db7b61b3
Joyson <antonyjoyson.dsouza@overtminds.com>
2026-07-24 15:24:55 +0530

    fix - routing back issue resolved

--- src/app/feature/job-details/job-details.ts
+++ src/app/feature/job-details/job-details.ts
@@ -9,10 +9,11 @@ import { formatJobType } from '../../shared/utils/formatJobType';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Location } from '@angular/common';
 import { NavigationService } from '../../core/shared/navigation';
+import { ShareJob } from '../../shared/components/share-job/share-job';
 
 @Component({
   selector: 'app-job-details',
-  imports: [RouterLink, SavedJobButton, ApplyJobButton, TranslatePipe],
+  imports: [RouterLink, SavedJobButton, ApplyJobButton, TranslatePipe, ShareJob],
   templateUrl: './job-details.html',
   styleUrl: './job-details.css',
 })
@@ -47,8 +48,9 @@ export class JobDetails {
 
   goBack() {
     const previousUrl = this.navigationService.getPreviousUrl();
-    
-    if (previousUrl) {
+    const currentUrl = this.navigationService.getCurrentUrl();
+
+    if (previousUrl && currentUrl != previousUrl) {
       this.router.navigateByUrl(previousUrl);
     } else {
       this.router.navigate(['/']);
